4edf5372e5fcf8c59cda467e761c2b07eb3aed91
arturo <arturo.tramontini@gmail.com>
2024-10-05 09:53:49 +0200

    --

--- app/main.py
+++ app/main.py
@@ -15,11 +15,7 @@ class App(Window):
 
 	def init(self):
 
-
-
-
-		logger.info(f'init')
-
+		logger.info(f"init")
 
 		# self.source = File('audio/gettysburg.wav')
 		self.source = Microphone()
@@ -29,18 +25,16 @@ class App(Window):
 		self.wave = Wave(self.ctx, 0, 0, config.WINDOW_WIDTH, 200)
 		self.nodes.append(self.wave)
 
-
 		self.spec = Spec(self.ctx, 0, self.wave.h, config.WINDOW_WIDTH, 460)
 		self.nodes.append(self.spec)
-
 
 		bg_color = (0.06, 0.06, 0.07, 1)
 		bg_color2 = (0.06, 0.06, 0.17, 1)
 
-
 		# Wave separation line
-		self.nodes.append(Rect(self.ctx, 0, self.wave.h, config.WINDOW_WIDTH, 2, bg_color))
-
+		self.nodes.append(
+			Rect(self.ctx, 0, self.wave.h, config.WINDOW_WIDTH, 2, bg_color)
+		)
 
 		# Time axis background
 		# self.r1 = Rect(self.ctx, 0, 660, config.WINDOW_WIDTH, 70, bg_color)
@@ -48,20 +42,23 @@ class App(Window):
 
 		# Y_db axis background
 		# self.r2 = Rect(self.ctx, 0, 0, 80, config.WINDOW_HEIGHT -72 , bg_color2)
-		self.nodes.append(Rect(self.ctx, 0, 0, 80, config.WINDOW_HEIGHT -72 , bg_color2))
+		self.nodes.append(
+			Rect(self.ctx, 0, 0, 80, config.WINDOW_HEIGHT - 72, bg_color2)
+		)
 
 		# Ticks
 
 		# 1/20th second ticks
 		self.nodes.append(
-			Ticks(self.ctx, x=81, y=660, w=1200, h=15, gap=6, color=(0.3,0.3,0.4,1))
+			Ticks(self.ctx, x=81, y=660, w=1200, h=15, gap=6, color=(0.3, 0.3, 0.4, 1))
 		)
 
 		# 1 second ticks
 		self.nodes.append(
-			Ticks(self.ctx, x=81, y=660, w=1200, h=25, gap=120, color=(0.4,0.4,0.5,1))
+			Ticks(
+				self.ctx, x=81, y=660, w=1200, h=25, gap=120, color=(0.4, 0.4, 0.5, 1)
+			)
 		)
-
 
 		# 2000 Hz ticks
 		pixels_per_freq = self.spec.h / 11046
@@ -72,12 +69,11 @@ class App(Window):
 				y=self.spec.y + pixels_per_freq * 1046,
 				w=10,
 				h=pixels_per_freq * 10000,
-				color=(0.4,0.4,0.5,1),
+				color=(0.4, 0.4, 0.5, 1),
 				gap=pixels_per_freq * 2000,
-				horizontal=False
+				horizontal=False,
 			)
 		)
-
 
 		# Text
 
@@ -88,17 +84,15 @@ class App(Window):
 		# Seconds text
 		for i in range(1, 11):
 			x = config.WINDOW_WIDTH - i * 120
-			text.add(f'{i}s', x, 705, align='center')
+			text.add(f"{i}s", x, 705, align="center")
 
 		# Hz text
 		for i in range(6):
 			hz = i * 2000
 			y = 660 - pixels_per_freq * hz + 4
-			text.add(f'{hz}hz', 62, y, align="right")
+			text.add(f"{hz}hz", 62, y, align="right")
 
-
-
-	def size(self,w,h):
+	def size(self, w, h):
 
 		# self.wave.size(w, h)
 		# self.spec.size(w, h)
@@ -107,12 +101,11 @@ class App(Window):
 		for node in self.nodes:
 			node.size(w, h)
 
-
 	def draw(self, dt):
 		available = self.source.available()
 		# window = self.source.get()
 
-		logger.info(f'{self.source.hop_cnt}, {self.source.hop_size}, {available}')
+		logger.info(f"{self.source.hop_cnt}, {self.source.hop_size}, {available}")
 		# logger.info(f'{self.source.hop_cnt}, {self.source.hop_size}, {available}, {window.shape if window is not None else None}')
 		# logger.info(f'{available}, {window.shape if window is not None else None}')
 
@@ -132,12 +125,10 @@ class App(Window):
 		for node in self.nodes:
 			node.draw()
 
-
 	def exit(self):
-		logger.info('exit')
+		logger.info("exit")
 		self.source.release()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 	App.run()
--- app/wave.py
+++ app/wave.py
@@ -6,7 +6,7 @@ from utils import orthographic
 
 class Wave:
 
-    VERTEX = """
+	VERTEX = """
 		#version 330 core
 		uniform mat4 P;
 		uniform float x, y, h;
@@ -18,7 +18,7 @@ class Wave:
 		}
 	"""
 
-    FRAGMENT = """
+	FRAGMENT = """
 		#version 330 core
 		out vec4 out_color;
 		void main(){
@@ -26,40 +26,40 @@ class Wave:
 		}
 	"""
 
-    def __init__(self, ctx, x, y, w, h):
-        self.ctx = ctx
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.prog = self.ctx.program(
-            vertex_shader=self.VERTEX, fragment_shader=self.FRAGMENT
-        )
-        self.samples = np.zeros(int(w * 2), dtype="f4")
-        self.buffer = self.ctx.buffer(reserve=self.samples.nbytes, dynamic=True)
-        self.vao = self.ctx.vertex_array(self.prog, self.buffer, "sample")
-        self.prog["x"] = x
-        self.prog["y"] = y
-        self.prog["h"] = h
-        self.sample = 0.002
-        self.update()
+	def __init__(self, ctx, x, y, w, h):
+		self.ctx = ctx
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.prog = self.ctx.program(
+			vertex_shader=self.VERTEX, fragment_shader=self.FRAGMENT
+		)
+		self.samples = np.zeros(int(w * 2), dtype="f4")
+		self.buffer = self.ctx.buffer(reserve=self.samples.nbytes, dynamic=True)
+		self.vao = self.ctx.vertex_array(self.prog, self.buffer, "sample")
+		self.prog["x"] = x
+		self.prog["y"] = y
+		self.prog["h"] = h
+		self.sample = 0.002
+		self.update()
 
-    def add(self, window):
-        if window is not None:
-            self.sample = np.abs(window[:100]).max()
-        #   sample = np.abs(window[:100]).max()
-        # else:
-        #   sample = 0.002
-        self.samples[:-2] = self.samples[2:]
-        # self.samples[-2:] = [-sample, sample]
-        self.samples[-2:] = [-self.sample, self.sample]
+	def add(self, window):
+		if window is not None:
+			self.sample = np.abs(window[:100]).max()
+		#   sample = np.abs(window[:100]).max()
+		# else:
+		#   sample = 0.002
+		self.samples[:-2] = self.samples[2:]
+		# self.samples[-2:] = [-sample, sample]
+		self.samples[-2:] = [-self.sample, self.sample]
 
-    def update(self):
-        self.buffer.write(self.samples)
+	def update(self):
+		self.buffer.write(self.samples)
 
-    def size(self, w, h):
-        P = orthographic(w, h)
-        self.prog["P"].write(P)
+	def size(self, w, h):
+		P = orthographic(w, h)
+		self.prog["P"].write(P)
 
-    def draw(self):
-        self.vao.render(moderngl.LINES)
+	def draw(self):
+		self.vao.render(moderngl.LINES)
